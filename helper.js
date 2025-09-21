0e563fcf9ef536904e36d0282d6a9066e8d383f0
Marko Nopri <mnopri82@gmail.com>
2025-09-21 14:00:09 +0300

    Added contact us, and modified homepage

--- helper.js
+++ helper.js
@@ -14,3 +14,21 @@ window.addEventListener('resize', function() {
         nav.classList.remove('open');
     }
 });
+
+const images = [
+  'assets/computer-2565478.jpg',
+  'assets/desk-1284085.jpg',
+  'assets/job-5382501.jpg'
+];
+
+let currentIndex = 0;
+const hero = document.querySelector('.hero');
+
+function changeBackground() {
+  hero.style.setProperty('--bg-image', `url(${images[currentIndex]})`);
+  currentIndex = (currentIndex + 1) % images.length;
+}
+
+// Alustetaan ensimmäinen kuva
+hero.style.setProperty('--bg-image', `url(${images[0]})`);
+setInterval(changeBackground, 5000); // vaihtuu 5 sekunnin välein
